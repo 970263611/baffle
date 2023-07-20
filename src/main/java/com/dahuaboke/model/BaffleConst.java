1dbc48c72dafb0674421a92900e71c0ee5649ee9
dahua <970263611@qq.com>
2023-07-20 19:24:20 +0800

    优化请求头下划线问题

--- src/main/java/com/dahuaboke/model/BaffleConst.java
+++ src/main/java/com/dahuaboke/model/BaffleConst.java
@@ -6,7 +6,7 @@ package com.dahuaboke.model;
  */
 public class BaffleConst {
 
-    public static final String BAFFLE_APPOINT_IP = "baffle_appoint_ip";
+    public static final String BAFFLE_APPOINT_IP = "baffle-appoint-ip";
 
     public static final String HTTP_PREFIX = "http://";
 
