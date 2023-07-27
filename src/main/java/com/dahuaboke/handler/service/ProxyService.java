a626fd3826194c8737a59a491b572e0a3202ac0c
dahua <970263611@qq.com>
2023-07-27 10:18:16 +0800

    删除无效打印

--- src/main/java/com/dahuaboke/handler/service/ProxyService.java
+++ src/main/java/com/dahuaboke/handler/service/ProxyService.java
@@ -3,8 +3,6 @@ package com.dahuaboke.handler.service;
 import com.dahuaboke.handler.net.RequestCallBack;
 import com.dahuaboke.handler.net.template.RequestTemplateFacade;
 import com.dahuaboke.model.BaffleResponse;
-import com.dahuaboke.model.HttpTemplateMode;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.netty.handler.codec.http.HttpMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -24,10 +22,8 @@ public class ProxyService {
     private RequestTemplateFacade requestTemplateFacade;
 
     public BaffleResponse proxy(String url, HttpMethod method, Map<String, String> headers, String body) throws ExecutionException, InterruptedException {
-        System.out.println(String.format("转发请求：url：%s，method：%s，headers：%s，body：%s", url, method, headers, body));
         CompletableFuture<BaffleResponse> completableFuture = new CompletableFuture();
         RequestCallBack requestCallBack = (baffleResponse) -> {
-            System.out.println("请求结果：" + baffleResponse);
             completableFuture.complete(baffleResponse);
         };
         requestTemplateFacade.exec(url, method, headers, body, requestCallBack);
